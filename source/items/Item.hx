bda50fdc335762a15712f15032fd8b38eaa40fd5
igna_torres92 <igna_torres92@hotmail.com>
2014-10-23 19:36:18 +0000

    prueba 2

--- source/items/Item.hx
+++ source/items/Item.hx
@@ -1,4 +1,4 @@
-package Items ;
+package items ;
 
 /**
  * ...
---
+++ source/items/food/Food.hx
@@ -0,0 +1,22 @@
+package items.food;
+import items.Item;
+
+/**
+ * ...
+ * @author ...
+ */
+class Food extends Item
+{
+	
+	//Stats
+	private var _HealthValue: Int;
+	private var _ArmorValue: Int;
+	private var _HungryValue: Int;
+	private var _EnergyValue: Int;
+
+	public function new() 
+	{
+		
+	}
+	
+}
